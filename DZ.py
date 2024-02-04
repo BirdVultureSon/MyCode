8207acb637794ae45247af9ed75ff742f5eebd23
BirdVultureSon <mizadoro@gmail.com>
2024-02-04 21:41:17 +0700

    Create DZ.py
    перенес свои данные в репу

---
+++ DZ.py
@@ -0,0 +1,29 @@
+
+wordList = ["яблоко", "груша", "банан"]
+import random
+wordSecret = random.choice(wordList)
+letters = []
+print (wordSecret) #Test random function
+choises = 5
+while choises > 0 :
+    if wordSecret != letters:
+        wordUser = input ("Введите вашу букву: ")
+        letters.append(wordUser)
+        print (letters)
+    
+
+        for i in wordSecret:
+                if i in letters:
+                    print (i, end ="")
+                else:
+                    print ("*", end ="")
+            
+    if wordSecret == letters:
+        print ("Вы угадали")
+        brake
+    if wordUser  not in wordSecret:
+        choises = choises - 1
+    
+else:
+    print ("Ваши попытки закончились")
+       
