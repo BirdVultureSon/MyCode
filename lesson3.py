a8e7635a0ffd9a5907f957e32d38bac45d8ea19a
BirdVultureSon <mizadoro@gmail.com>
2024-02-04 21:41:40 +0700

    Create lesson3.py
    перенес свои данные в репу

---
+++ lesson3.py
@@ -0,0 +1,29 @@
+import math 
+'''city = "Барнаул"
+print (city, "очень красивый город")'''
+'''year = int(input("В каком году ты родился?: "))
+print ("в 2035 году тебе исполнится : " + str(2035 - year))'''
+'''boolean = 5 == 3
+print (boolean)'''
+'''city = "Барнаул"
+year = 2010
+print (f"я хочу поситить {city} в {year}")'''
+'''x = float(input("Введите сторону квадрата: "))
+#y
+per = x * 4
+area = x * x
+print (f"Периметр {per}, Площадь {area}")'''
+'''x = float(input("Введите сторону 1: "))
+y = float(input("Введите сторону 2: "))
+per = (x + y) * 2
+area = x * y 
+print (f"Периметр {per}, Площадь {area}")'''
+print (10/3)
+x = float(input("Введите сторону 1: "))
+y = float(input("Введите сторону 2: "))
+per = (x + y) * 2
+area = x * y
+diag = math.sqrt (2 * x * y)
+print (f"Диаг {diag}")
+print (f"Периметр {per}, Площадь {area}")
+
