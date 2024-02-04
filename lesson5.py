dbefac2df4ff3aa9c5d9c2c1d5f833130a5dd233
BirdVultureSon <mizadoro@gmail.com>
2024-02-04 21:41:46 +0700

    Create lesson5.py
    перенес свои данные в репу

---
+++ lesson5.py
@@ -0,0 +1,26 @@
+#Третья методичка
+age = 14
+temp = 0
+things = "На столе лежат тетрадь и карандаш"
+print ((age < 14))
+'''if 7 <= age <= 17:
+    print ("Тебе от 7 до 17 лет")'''
+'''if "тетрад " in things :
+    print ("Сработало")'''
+'''if temp < 0:
+    print ("Очень холодно")
+elif 0 <= temp < 15:
+    print ("Прохладно")
+elif temp >= 15:
+    print ("Тепло")
+elif temp <= 0:
+    print ("Очень холодно")'''
+money = False
+position = True
+if money == True or position == True:
+    print ("Мы заняли место в автобусе")
+else: 
+    print ("Ты не занял место")
+
+
+    
