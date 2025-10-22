e794be3a61628dd752a202eb7483242ffdbd138e
BirdVultureSon <158830796+BirdVultureSon@users.noreply.github.com>
2025-10-23 00:52:18 +0700

    приближенная версия к работе
    ошибки в коментах

--- project/prog.py
+++ project/prog.py
@@ -2,10 +2,23 @@ import sqlite3 as sql3
 import tkinter as tk
 import os
 
-#блок создания базы данных
+''''
+Проблемы:
+1) кнопки по какой то причине прожимаются автоматически после запуска программы
+
+
+
+
+
+'''
+
+
+#I СОЗДАНИЕ БД
+
 
 db_file = "user_data.db"
 
+#блок проверки бд
 if not os.path.exists(db_file):
     database = sql3.connect(db_file)
 
@@ -18,39 +31,69 @@ if not os.path.exists(db_file):
 
     )""")
 
+    database.commit()
 
 else:
     database = sql3.connect(db_file)
 
+    cursor = database.cursor()
+
+    database.commit()
 
 
-database.commit()
+#II ФУНКЦИИ
 
-database.close()
+#функция сохранения
+def save():
+
+    sql_zapros = "INSERT INTO usersdata (password) VALUES(?)"
+
+    take_text = text.get()
+
+    cursor.execute(sql_zapros, (take_text, ))
+    print("work")
+
+    database.commit()
+
+#функция просмотра
+def look_database():
+
+    root_look = tk.Tk()
+    root_look.geometry("300x300")
+
+    key = "SELECT * FROM usersdata"
+
+    cursor.execute(key)
+
+    data = cursor.fetchall()
+
+    data_from_db = tk.Entry(root_look)
+
+    data_from_db.insert(0, str(data))
+
+    data_from_db.pack(padx= 150, pady= 150)
+
+    database.commit()
+    database.close()
 
 
+    root_look.mainloop()
 
-#блок программы
+
+#III ОКНО
 root = tk.Tk()
 
 root.title("Регистрация")
 root.geometry("600x600")
 text = tk.Entry(root)
 text.pack(padx = 150, pady = 150)
-but_to_save = tk.Button(root, text= "сохранить в бд", width= 15, height=3)
-but_to_save.pack(padx = 100, pady= 100)
+but_to_save = tk.Button(root, text= "сохранить в бд", width= 15, height=3, command=save())
+but_to_save.pack(padx = 3, pady= 3)
 
-but_to_get = tk.Button(root, text = "войти в бд", width= 15, height= 3)
-but_to_get.pack(padx = 80 , pady= 80)
 
-def save():
-    data = text.get
-    sql_work = "INSERT INTO userdata (password) VALUES(?,?)"
-    cursor.execute(sql_work, (data))
+but_to_look = tk.Button(root, text = "просмотреть бд", width= 15, height= 3, command=look_database())
+but_to_look.pack(padx = 80, pady = 80)
 
-    database.commit()
-    database.close()
-
-    root.destroy()
 
 root.mainloop()
+
