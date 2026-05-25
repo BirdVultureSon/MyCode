610eae86524abeea1862a66df2be8d5075d5eb3c
BirdVultureSon <158830796+BirdVultureSon@users.noreply.github.com>
2026-05-25 23:58:40 +0700

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(1216, 532);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(8, 31);
+			this.textBox1.TabIndex = 0;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(50, 59);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(244, 31);
+			this.textBox2.TabIndex = 2;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Low",
+            "Medium",
+            "High"});
+			this.comboBox1.Location = new System.Drawing.Point(482, 57);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 33);
+			this.comboBox1.TabIndex = 3;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(45, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 25);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Name";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(477, 22);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(80, 25);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Prioriry";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(754, 58);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(129, 33);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "add";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(50, 129);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(833, 536);
+			this.checkedListBox1.TabIndex = 7;
+			this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(258, 760);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(139, 40);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Remove";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(50, 760);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(139, 40);
+			this.button3.TabIndex = 9;
+			this.button3.Text = "Mark Done";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(464, 760);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(139, 40);
+			this.button4.TabIndex = 10;
+			this.button4.Text = "Claer all";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(977, 888);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.checkedListBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			if (comboBox1.Items.Count == 0)
+			{
+				comboBox1.Items.AddRange(new object[] { "Low", "Medium", "High" });
+				comboBox1.SelectedIndex = 1;
+			}
+
+			checkedListBox1.ItemCheck += CheckedListBox1_ItemCheck;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string task = textBox2.Text.Trim();
+			string priority = comboBox1.Text;
+
+			if (string.IsNullOrEmpty(task))
+			{
+				MessageBox.Show("Введите задачу.");
+				return;
+			}
+
+			checkedListBox1.Items.Add($"[ ] {priority} - {task}");
+			textBox2.Clear();
+		}
+
+		private void CheckedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+		{
+			this.BeginInvoke(new Action(() => {
+				if (e.NewValue == CheckState.Checked)
+				{
+					string text = checkedListBox1.Items[e.Index].ToString();
+
+					if (!text.StartsWith("[✓] "))
+					{
+						checkedListBox1.Items[e.Index] = text.Replace("[ ] ", "[✓] ");
+					}
+
+					checkedListBox1.Items.RemoveAt(e.Index);
+				}
+			}));
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int index = checkedListBox1.SelectedIndex;
+			if (index < 0) return;
+
+			object item = checkedListBox1.Items[index];
+			string text = item.ToString();
+
+			if (text.StartsWith("[ ] "))
+			{
+				checkedListBox1.Items[index] = text.Replace("[ ] ", "[✓] ");
+				checkedListBox1.SetItemChecked(index, true);
+			}
+			else if (text.StartsWith("[✓] "))
+			{
+				checkedListBox1.Items[index] = text.Replace("[✓] ", "[ ] ");
+				checkedListBox1.SetItemChecked(index, false);
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			int index = checkedListBox1.SelectedIndex;
+			if (index >= 0)
+			{
+				checkedListBox1.Items.RemoveAt(index);
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			checkedListBox1.Items.Clear();
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e) { }
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+		private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+	}
+}
